06f3fe86aa17d018c669d10baffb0ef2e3fe0363
sunjunpeng <sunisok1@gmail.com>
2024-07-29 19:03:46 +0800

    MathUtil

--- Assets/Scripts/Game/Core/Map/ChessMap.cs
+++ Assets/Scripts/Game/Core/Map/ChessMap.cs
@@ -1,14 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Framework.Singletons;
 using UnityEngine;
 
 namespace Game.Core.Map
 {
+    public enum GridState
+    {
+        None,
+        Pointer,
+        Selectable
+    }
+
     public class Chess : Singleton<Chess>
     {
+        public event Action<Vector2, GridState> OnGridStateChange;
+
         public async Task<Vector2> SelectPosition(Vector2 origin, int range)
         {
-            throw new System.NotImplementedException();
+            for (int i = -range; i <= range; i++)
+            {
+                for (int j = -range; j <= range; j++)
+                {
+                    if (Utils.MathUtil.ManhattanDistance(origin, origin + new Vector2(i, j)) > range)
+                    {
+                    }
+                }
+            }
+
+            OnGridStateChange
         }
     }
 }
---
+++ Assets/Scripts/Game/Core/Utils/MathUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Core.Utils
+{
+    public static class MathUtil
+    {
+        public static float ManhattanDistance(this Vector2 pos, Vector2 other)
+        {
+            return Math.Abs(pos.x - other.x) + Math.Abs(pos.y - other.y);
+        }
+    }
+}
